add528d246b2dfb8a13d03d7fdbf26b12565bc22
OliverHavis <s5421075@bournemouth.ac.uk>
2023-05-16 00:51:20 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # PSM-Travel-App
+Developed By Oliver Havis (S5421075) Group 9
+
+## Project Description
+This project is a travel app that allows users to search for a location and booking holidays.
+
+## Key Features
+- Account creation and login/logout
+- Encryption of all sensitive data
+- View and edit account details (with profile picture available)
+- Add, Remove and Favorite Cards
+- View Tops Destinations
+- Search for a location
+- Save a location/View all Saved Locations (includes images)
+- Select a location and see excursions available
+- Review/Make a booking
+- View all bookings
+- Countdown to next holiday
+
+## Incomplete Features
+- Cruse Section (Viewing, booking and saving)
+- Password Reset/Change
+- Search functionality (Search for a location)
+
+## Backend Details
+Google Firebase is used for the backend of this project.
+### Authentication
+Firebase Authentication is used to handle all user authentication. This includes account creation, login and logout.
+### Database
+Firebase Firestore is used to store all user data. This includes account details, saved locations, saved cards and bookings.
+The encrypted Data:
+- Account Details
+- Card Details
+### Storage
+Firebase Storage is used to store all images used in the app. This includes location images and excursion images.
+
--- app/src/main/java/com/example/psm/MainActivity.kt
+++ app/src/main/java/com/example/psm/MainActivity.kt
@@ -29,10 +29,6 @@ class MainActivity : ComponentActivity() {
     // initiaization
     private lateinit var db: FirebaseHelper
     private var user : User? = null
-
-    private var isHandburgerMenuOpen = false
-    private lateinit var hamburgerMenu: ImageButton
-    private lateinit var hiddenMenu: LinearLayout
     private lateinit var toolbar: Toolbar
     private lateinit var searchIcon: ImageButton
     private lateinit var searchEditText: EditText
@@ -50,8 +46,6 @@ class MainActivity : ComponentActivity() {
 
         // varaible declaration
         db = FirebaseHelper()
-        hamburgerMenu = findViewById<ImageButton>(R.id.hamburger_menu)
-        hiddenMenu = findViewById<LinearLayout>(R.id.hidden_menu)
         toolbar = findViewById<Toolbar>(R.id.toolbar)
         searchIcon = findViewById<ImageButton>(R.id.search_icon)
         searchEditText = findViewById<EditText>(R.id.searchEditText)
@@ -68,68 +62,6 @@ class MainActivity : ComponentActivity() {
         setupUI()
         setupListeners()
 
-    }
-
-    /**
-     * Opens hamburger menu.
-     */
-    private fun openMenu() {
-        val drawableResId = R.drawable.avd_anim_menu_close
-        runMenuAnimation(drawableResId)
-
-        hiddenMenu.measure(View.MeasureSpec.makeMeasureSpec(toolbar.measuredWidth, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
-        val targetHeight = hiddenMenu.measuredHeight
-
-        hiddenMenu.layoutParams.height = 0
-        hiddenMenu.visibility = View.VISIBLE
-
-        val animator = ValueAnimator.ofInt(0, targetHeight)
-        animator.duration = 300
-        animator.addUpdateListener { animation ->
-            val value = animation.animatedValue as Int
-            val layoutParams = hiddenMenu.layoutParams
-            layoutParams.height = value
-            hiddenMenu.layoutParams = layoutParams
-        }
-        animator.start()
-    }
-
-    /**
-     * Closes hamburger menu.
-     */
-    private fun closeMenu(wasScreenInput : Boolean = false) {
-        if (!wasScreenInput) {
-            val drawableResId = R.drawable.avd_anim_menu_open
-            runMenuAnimation(drawableResId)
-        }
-
-        val initialHeight = hiddenMenu.measuredHeight
-        val animator = ValueAnimator.ofInt(initialHeight, 0)
-        animator.duration = 300
-        animator.addUpdateListener { animation ->
-            val value = animation.animatedValue as Int
-            val layoutParams = hiddenMenu.layoutParams
-            layoutParams.height = value
-            hiddenMenu.layoutParams = layoutParams
-        }
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator) {
-                hiddenMenu.visibility = View.GONE
-            }
-        })
-        animator.start()
-    }
-
-    /**
-     * Updates the handburger menu animation.
-     */
-    private fun runMenuAnimation(drawableResId : Int) {
-        hamburgerMenu.setImageResource(drawableResId)
-
-        val drawable = hamburgerMenu.drawable
-        if (drawable is Animatable) {
-            (drawable as Animatable).start()
-        }
     }
 
     /**
@@ -224,27 +156,8 @@ class MainActivity : ComponentActivity() {
          * Closes the menu and the search bar when the screen is clicked.
          */
         screen.setOnClickListener {
-            // Close Menu
-            closeMenu(true)
-            isHandburgerMenuOpen = false
-
             // Close Search
             closeSearch()
-        }
-
-        // Handburger Menu Listeners
-        /**
-         * Opens/Closes the hamburger menu.
-         */
-        hamburgerMenu.setOnClickListener {
-
-            if (isHandburgerMenuOpen) {
-                closeMenu()
-            } else {
-                openMenu()
-            }
-
-            isHandburgerMenuOpen = !isHandburgerMenuOpen
         }
 
         /**
@@ -318,9 +231,6 @@ class MainActivity : ComponentActivity() {
     override fun onResume() {
         super.onResume()
         setupUI()
-        // Close Menu
-        closeMenu(true)
-        isHandburgerMenuOpen = false
 
         // Close Search
         closeSearch()
